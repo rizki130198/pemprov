6789a80f310e76e35f4a55e5a3c354b9ddbe160d
rizki130198 <rizkiajja130198@gmail.com>
2018-09-01 01:12:15 +0700

    update

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Http\Controllers;
 
-
 use Auth;
 use Hash;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
+use App\Models\User;
+use App\Models\UserDirectMessage;
 use Session;
 
 class SettingsController extends Controller
@@ -19,20 +21,34 @@ class SettingsController extends Controller
 
 
     public function index(){
-
+        $response = array();
+        $response['code'] = 200;
 
         if (Session::has('user')){
             $user = Session::get('user');
         }else{
             $user = Auth::user();
         }
+        $user_list = [];
 
+        $message_list = DB::select( DB::raw("select * from (select * from `user_direct_messages` where `receiver_user_id` = '".$user->id."' and `receiver_delete` = '0'  and `seen` = '0' order by `id` desc limit 200000) as group_table group by sender_user_id order by id desc") );
 
+        $new_list = [];
+        foreach(array_reverse($message_list) as $list){
+            $msg = new UserDirectMessage();
+            $msg->dataImport($list);
+            $new_list[] = $msg;
+        }
 
+        foreach (array_reverse($new_list) as $message){
+            $user_list[$message->sender_user_id] = [
+                'new' => ($message->seen == 0)?true:false,
+                'message' => $message,
+                'user' => $message->sender
+            ];
+        }
 
-        return view('settings', compact('user'));
-
-
+        return view('settings', compact('user','user_list'));
     }
 
     public function update(Request $request){
