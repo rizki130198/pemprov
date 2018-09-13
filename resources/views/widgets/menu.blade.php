84dbad56cc552abf4c63ed9c6b7340565b8aa191
rizki130198 <rizkiajja130198@gmail.com>
2018-09-13 14:59:07 +0700

    update

--- resources/views/widgets/menu.blade.php
+++ resources/views/widgets/menu.blade.php
@@ -2,7 +2,9 @@
     <h4 style="color: #61677;margin-bottom: 20px; font-size: 15px;letter-spacing: 1px;margin-left: 60px;"><strong>Jelajahi</strong></h4>
     <ul class="menu-explore">
         <a href="{{ url('/') }}"><li class="{{ request()->is('home') ? 'active' : '' }}"><i class="fa fa-home" style="color: #339399;"></i>Home</li></a>
-        <a href="{{ url('/events') }}"><li class="{{ request()->is('events') ? 'active' : '' }}"><i class="fa fa-map-marker" style="color: #abc554;"></i>Events <span class="badge" style="float: right;background-color: #abc554;">{{$data->count()}}</span></li></a>
+        <a href="{{ url('/events') }}">
+        	<li class="{{ request()->is('events') ? 'active' : '' }}"><i class="fa fa-map-marker" style="color: #abc554;"></i>Events <span class="badge" style="float: right;background-color: #abc554;">{{$data->count()}}</span></li>
+        </a>
         <a href="{{ url('/groups') }}"><li class="{{ Request::segment(1) == 'group' || Request::segment(1) == 'groups' ? 'active' : '' }}"><i class="fa fa-users" style="color:#e17e41;"></i>Groups</li></a>
         <a href="{{ url('/direct-messages') }}"><li class="{{ request()->is('direct-messages') ? 'active' : '' }}"><i class="fa fa-commenting" style="color: #d5483c;"></i>Direct Messages</li></a>
     </ul>
